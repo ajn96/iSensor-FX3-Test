07f43261efee0928782047664f7e789336382b5b
anolan3 <Alex.Nolan@analog.com>
2020-05-15 13:51:31 -0400

    Added connect/disconnect test

--- Test/FunctionalTests.cs
+++ Test/FunctionalTests.cs
@@ -15,6 +15,21 @@ namespace iSensor_FX3_Test
         public void FirmwareLoadTest()
         {
             Connect();
+
+            const int RESET_TRIALS = 10;
+
+            string sn = FX3.ActiveFX3.SerialNumber;
+
+            for(int trial = 0; trial < RESET_TRIALS; trial++)
+            {
+                Console.WriteLine("Disconnecting FX3...");
+                FX3.Disconnect();
+                Assert.IsNull(FX3.ActiveFX3, "ERROR: Active board should be null after disconnect");
+                FX3.WaitForBoard(5);
+                Assert.AreEqual(1, FX3.AvailableFX3s.Count, "ERROR: Expected only 1 FX3 to be available...");
+                Console.WriteLine("Connecting to FX3...");
+                FX3.Connect(sn);
+            }
         }
 
         [Test]
