affa7003f817e4a42ec21ba46b6ba05448da834d
anolan3 <Alex.Nolan@analog.com>
2020-05-19 16:59:47 -0400

    Moved result files to results folder

--- build_script.bat
+++ build_script.bat
@@ -1,18 +1,16 @@
-@echo off
+@echo Build started > Results\build_log.txt
 
-@echo Build started > build_log.txt
-
-@echo Updating test repository... >> build_log.txt
+@echo Updating test repository... >> Results\build_log.txt
 git pull
 
-@echo Removing existing build artifacts >> build_log.txt
+@echo Removing existing build artifacts >> Results\build_log.txt
 
 rmdir /S /Q %CD%\iSensor-FX3-API\bin
 rmdir /S /Q %CD%\Resources
 rmdir /S /Q %CD%\Test\bin
 copy /y %CD%\build_failed.png %CD%\test_status.png
 
-@echo Fetching fresh copy of API... >> build_log.txt
+@echo Fetching fresh copy of API... >> Results\build_log.txt
 
 IF EXIST %CD%\iSensor-FX3-API (
 cd iSensor-FX3-API\
@@ -23,33 +21,33 @@ cd..
 git clone https://github.com/juchong/iSensor-FX3-API.git
 )
 
-@echo Building FX3-API... >> build_log.txt
+@echo Building FX3-API... >> Results\build_log.txt
 cd iSensor-FX3-API\
 msbuild FX3Api.vbproj /p:configuration=debug
 cd..
 
-@echo Copying output... >> build_log.txt
+@echo Copying output... >> Results\build_log.txt
 mkdir %CD%\Resources
 xcopy /s /Y %CD%\iSensor-FX3-API\bin\Debug %CD%\Resources
 xcopy /s /Y /i %CD%\iSensor-FX3-API\Resources\boot_fw.img %CD%\Resources
 xcopy /s /Y /i %CD%\iSensor-FX3-API\Resources\FX3_Firmware.img %CD%\Resources
 xcopy /s /Y /i %CD%\iSensor-FX3-API\Resources\USBFlashProg.img %CD%\Resources
 
-@echo Building tests... >> build_log.txt
+@echo Building tests... >> Results\build_log.txt
 cd Test\
 msbuild iSensor-FX3-Test.csproj /p:configuration=debug
 cd..
 
 @echo Running tests...
-NUnit-2.6.4\bin\nunit-console-x86.exe /framework:net-4.5 /out:%CD%\test_console.txt /xml:%CD%\test_result.xml %CD%\Test\bin\Debug\iSensor-FX3-Test.dll >> build_log.txt
+NUnit-2.6.4\bin\nunit-console-x86.exe /framework:net-4.5 /out:%CD%\Results\test_console.txt /xml:%CD%\Results\test_result.xml %CD%\Test\bin\Debug\iSensor-FX3-Test.dll >> Results\build_log.txt
 
-@echo Parsing test result... >> build_log.txt
-NUnitLogParser.exe %CD%\test_result.xml %CD%\test_status.png -verbose  >> build_log.txt
+@echo Parsing test result... >> Results\build_log.txt
+NUnitLogParser.exe %CD%\test_result.xml %CD%\test_status.png -verbose  >> Results\build_log.txt
 
 @echo Pushing results...
-git add test_result.xml
-git add test_console.txt
-git add test_status.png
-git add build_log.txt
+git add Results\test_result.xml
+git add Results\test_console.txt
+git add Results\test_status.png
+git add Results\build_log.txt
 git commit -m "Test run results, %DATE% %TIME%"
 git push
