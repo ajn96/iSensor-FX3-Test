1c63d5ff58624def8fb7306c4b90d33f629827a9
anolan3 <Alex.Nolan@analog.com>
2020-05-19 15:38:13 -0400

    Added test status generation to build script

--- build_script.bat
+++ build_script.bat
@@ -38,8 +38,12 @@ cd..
 @echo Running tests...
 NUnit-2.6.4\bin\nunit-console-x86.exe /framework:net-4.5 /out:%CD%\test_console.txt /xml:%CD%\test_result.xml %CD%\Test\bin\Debug\iSensor-FX3-Test.dll
 
+@echo Parsing test result...
+NUnitLogParser.exe %CD%\test_result.xml %CD%\test_status.png -verbose
+
 @echo Pushing results...
 git add test_result.xml
 git add test_console.txt
+git add test_status.png
 git commit -m "Test run results, %DATE% %TIME%"
 git push
